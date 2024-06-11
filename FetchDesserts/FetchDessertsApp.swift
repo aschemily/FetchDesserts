3b7237b5ff71399095776322a483cb07f395487c
aschemily <aschemily@gmail.com>
2024-06-11 12:23:52 -0400

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Emily Asch on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
